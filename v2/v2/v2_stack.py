43c29301d842fd98881a316e9365ac1d3270193a
root <root@ip-172-31-86-115.ec2.internal>
2023-03-06 13:14:14 +0000

    cleanup

--- v2/v2/v2_stack.py
+++ v2/v2/v2_stack.py
@@ -2,10 +2,6 @@ from constructs import Construct
 from aws_cdk import (
     Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
     aws_s3 as s3,
 )
 
@@ -16,7 +12,7 @@ class V2Stack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        b = s3.Bucket(
+        s3.Bucket(
                 self,
                 'cdk-v2stack-qwert098',
                 auto_delete_objects=True,
@@ -24,13 +20,3 @@ class V2Stack(Stack):
                 bucket_name = 'cdk-v2stack-qwert098'
                 )
 
-#        queue = sqs.Queue(
-#            self, "V2Queue",
-#            visibility_timeout=Duration.seconds(300),
-#        )
-#
-#        topic = sns.Topic(
-#            self, "V2Topic"
-#        )
-#
-#        topic.add_subscription(subs.SqsSubscription(queue))
